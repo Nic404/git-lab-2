03f0fdc5446985baed75b7ad43b7e82964f54ab1
Nic Moorman <nm649717@ohio.edu>
2020-01-24 17:03:25 -0500

    First Program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nic Moorman>
+ *        Date: <1/24/2000>
+ * Description: Hello World for git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
